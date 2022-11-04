3e619c30025e5fab66899c92b387e6c609f62c80
James <entreur@gmail.com>
2022-11-04 12:15:39 +0400

    Initial Commit

--- fossmusic/lib/main.dart
+++ fossmusic/lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:fossmusic/UI/Home.dart';
+import 'package:flutter_exoplayer/audioplayer.dart';
 
 void main() {
+  AudioPlayer.logEnabled = true;
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Tutorial',
+      home: Home(),
+    ),
+  );
 }
+
