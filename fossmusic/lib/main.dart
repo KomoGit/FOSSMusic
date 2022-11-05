a8a7e4e65d5d25a3da854ef4fca83cd0d7e1751a
James <entreur@gmail.com>
2022-11-05 16:46:29 +0400

    Small Changes.

--- fossmusic/lib/main.dart
+++ fossmusic/lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fossmusic/UI/ui_home.dart';
-import 'package:flutter_exoplayer/audioplayer.dart';
 
 void main() {
-  AudioPlayer.logEnabled = true;
+  //AudioPlayer.logEnabled = true;
   runApp(const Home());
 }
 
