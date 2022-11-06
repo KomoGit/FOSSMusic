9dd8a506d7207535768126edf95e778d59b8e5bf
James <entreur@gmail.com>
2022-11-06 18:48:57 +0400

    Cleanup

--- fossmusic/lib/UI/ui_tindercard.dart
+++ fossmusic/lib/UI/ui_tindercard.dart
@@ -3,11 +3,6 @@ import 'package:swipable_stack/swipable_stack.dart';
 
 
 final controller = SwipableStackController();
-var imageURL = <String>{
-  "https://wallpaper.dog/download-wallpaper/20521641_1080x1920",
-  "https://wallpaper.dog/download-wallpaper/20521639_1080x1920",
-  "https://wallpaper.dog/download-wallpaper/20521642_1080x1920",
-};
 //With each change to code base, the card stack becomes unresponsive during debugging. Decouple UI from main thread.
 
 class TinderCard extends StatelessWidget {
@@ -15,9 +10,9 @@ class TinderCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      backgroundColor: Colors.transparent,
       body: SwipableStack(builder: (context,index){
-          return Image.network(imageURL.first); //REMOVE ME
+          return Image.asset("assets/WallpaperDog-20521639.jpg"); //REMOVE ME
         },
         controller: controller,
         detectableSwipeDirections: const{
