d75beb59fcff725f8c32742b50dad17f9b59194d
James <entreur@gmail.com>
2022-11-09 20:09:28 +0400

    UI for playlist.

---
+++ fossmusic/lib/UI/ui_playlist.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PlayListView extends StatefulWidget {
+  const PlayListView({super.key});
+
+  @override
+  State<PlayListView> createState() => _PlayListViewState();
+}
+
+class _PlayListViewState extends State<PlayListView> {
+  @override
+  Widget build(BuildContext context) {
+    return  const Scaffold(
+      backgroundColor: Colors.yellow,
+    );
+  }
+}
