35fc0cc9a38682de6b92e7a1a09338c3a30f2e63
James <entreur@gmail.com>
2022-11-10 19:19:34 +0400

    Input for users to add songs to playlist.

---
+++ fossmusic/lib/UI/ui_input.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UserInputPopUp extends StatelessWidget {
+  const UserInputPopUp({super.key});
+//DOESNT WORK
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+     // backgroundColor: Color.fromARGB(75, 255, 193, 7),
+      backgroundColor: Colors.white,
+    );
+  }
+}
