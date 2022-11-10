2b3cc52d12863d53450a4af37367fcf800175b21
James <entreur@gmail.com>
2022-11-10 18:57:02 +0400

    CRUD Implementation.

--- fossmusic/lib/DB/db_controller.dart
+++ fossmusic/lib/DB/db_controller.dart
@@ -4,15 +4,15 @@ import 'package:fossmusic/DB/models/model_song.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
-
-class DatabaseHelper{
+//CREDIT : https://www.youtube.com/watch?v=noi6aYsP7Go&list=WL&index=2
+class DatabaseHelper {
   DatabaseHelper._privateConstructor();
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
   static Database? _db;
   Future<Database> get _database async => _db ??= await _initDatabase();
 
-  Future<Database> _initDatabase() async{
+  Future<Database> _initDatabase() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentsDirectory.path, "FInternalDB.db");
     return openDatabase(
@@ -22,15 +22,35 @@ class DatabaseHelper{
     );
   }
 
-  Future _onCreate(Database db,int version)async{
-    await db.execute('CREATE TABLE songs (id INTEGER PRIMARY KEY, link TEXT)');
+  Future _onCreate(Database db, int version) async {
+    await db.execute(
+        'CREATE TABLE songs (id INTEGER PRIMARY KEY,uuid TEXT,link TEXT)');
   }
 
-  Future<List<Song>> getSongs()async{
+  //C.R.U.D
+
+  //CREATE
+  Future<int> add(Song song) async {
     Database db = await instance._database;
-    var songs = await db.query('songs',orderBy: 'id');
-    List<Song> songList = songs.isNotEmpty
-      ? songs.map((e) => Song.fromMap(e)).toList() : [];
+    return await db.insert('songs', song.toMap());
+  }
+  //READ
+  Future<List<Song>> getSongs() async {
+    Database db = await instance._database;
+    var songs = await db.query('songs', orderBy: 'id');
+    List<Song> songList =
+        songs.isNotEmpty ? songs.map((e) => Song.fromMap(e)).toList() : [];
     return songList;
+  }
+  //UPDATE (NOT IMPLEMENTED YET)
+  Future<int> update(Song song) async {
+    Database db = await instance._database;
+    return await db.update('groceries', song.toMap(),
+        where: "id = ?", whereArgs: [song.id]);
+  }
+  //DELETE
+    Future<int> remove(int id) async {
+    Database db = await instance._database;
+    return await db.delete('songs', where: 'id = ?', whereArgs: [id]);
   }
 }
