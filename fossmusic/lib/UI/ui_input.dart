3cd775ddce4ffc95054541e1eda98c73bbf45ef8
James <entreur@gmail.com>
2022-11-11 11:39:15 +0400

    Input box.

--- fossmusic/lib/UI/ui_input.dart
+++ fossmusic/lib/UI/ui_input.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:fossmusic/DB/db_controller.dart';
+import 'package:fossmusic/DB/models/model_song.dart';
 
 class UserInputPopUp extends StatelessWidget {
   const UserInputPopUp({super.key});
 //DOESNT WORK
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-     // backgroundColor: Color.fromARGB(75, 255, 193, 7),
-      backgroundColor: Colors.white,
+    TextEditingController nameController = TextEditingController();
+    return AlertDialog(
+      title: const Text('Add song to playlist'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+                onChanged: (v) => nameController.text = v,
+                decoration: const InputDecoration(
+                  labelText: 'Insert Link',
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Builder(
+              builder: (context) {
+                return ElevatedButton(
+                  onPressed: () async {
+                    await DatabaseHelper.instance.add(Song(link: nameController.text));
+                  },
+                  child: const Text('Submit'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
