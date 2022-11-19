76ebd62cdd4c61e1d53a6ed19f2c3e8acba45e0b
James <entreur@gmail.com>
2022-11-19 12:37:41 +0400

    Ability to play songs.

--- fossmusic/lib/UI/ui_playlist.dart
+++ fossmusic/lib/UI/ui_playlist.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fossmusic/DB/db_controller.dart';
 import 'package:fossmusic/DB/models/model_song.dart';
+import 'package:fossmusic/LOGIC/logic_player.dart';
 
 class PlayListView extends StatefulWidget {
   const PlayListView({super.key});
@@ -12,33 +13,40 @@ class PlayListView extends StatefulWidget {
 class _PlayListViewState extends State<PlayListView> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Center(
-          child: FutureBuilder<List<Song>>(
-              future: DatabaseHelper.instance.getSongs(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<List<Song>> snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(child: Text('Loading...'));
-                }
-                return snapshot.data!.isEmpty ? const Center(child: Text("No songs added to the playlist."),)
-                // Add padding to each of the items. And a visible border.
-                : ListView(
-                  children: snapshot.data!.map((song) {
-                    return Center(
-                      child: ListTile(
-                        title: Text(song.link),//Should be combo of two strings, Singer - Song name
-                        onLongPress: (){
-                          setState(() {
-                            DatabaseHelper.instance.remove(song.id!);
-                          });
-                        },
-                      ),
+        child: FutureBuilder<List<Song>>(
+            future: DatabaseHelper.instance.getSongs(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Song>> snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: Text('Loading...'));
+              }
+              return snapshot.data!.isEmpty
+                  ? const Center(
+                      child: Text("No songs added to the playlist."),
+                    )
+                  // Add padding to each of the items. And a visible border.
+                  : ListView(
+                      children: snapshot.data!.map((song) {
+                        return Center(
+                          child: ListTile(
+                            title: Text(song
+                                .link), //Should be combo of two strings, Singer - Song name
+                            onLongPress: () {
+                              setState(() {
+                                DatabaseHelper.instance.remove(song.id!);
+                              });
+                            },
+                            onTap: () {
+                              playSong(song.link);
+                            },
+                          ),
+                        );
+                      }).toList(),
                     );
-                  }).toList(),
-                );
-              }),
-        ), 
+            }),
+      ),
       backgroundColor: Colors.yellow,
     );
   }
