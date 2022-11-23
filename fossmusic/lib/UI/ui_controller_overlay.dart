c963dfe3d854f7aa68de60688a760a31c877cba6
James <entreur@gmail.com>
2022-11-23 12:05:36 +0400

    Pause and play song.

--- fossmusic/lib/UI/ui_controller_overlay.dart
+++ fossmusic/lib/UI/ui_controller_overlay.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fossmusic/LOGIC/logic_player.dart';
 import 'package:fossmusic/UI/ui_tindercard.dart';
 import 'package:swipable_stack/swipable_stack.dart';
+
+bool pausePressed = false;
 
 class OverlayController extends StatelessWidget {
   const OverlayController({super.key});
@@ -12,38 +15,63 @@ class OverlayController extends StatelessWidget {
       children: [
         ElevatedButton(
             onPressed: () {
-              TinderCard.controller.next(swipeDirection: SwipeDirection.left,duration: const Duration(milliseconds: 750));
+              TinderCard.controller.next(
+                  swipeDirection: SwipeDirection.left,
+                  duration: const Duration(milliseconds: 750));
             },
             style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                minimumSize: const Size(60, 60),
+                shape: const CircleBorder()),
+            child: const Icon(
+              Icons.skip_previous,
+              color: Colors.white,
+            )),
+        ElevatedButton(
+          onPressed: () {
+            if (!pausePressed) {
+              pausePressed = true;
+              pauseSong();
+              //continueSong();
+            } else {
+              pausePressed = false;
+              continueSong();
+              //pauseSong();
+            }
+          },
+          style: ElevatedButton.styleFrom(
               backgroundColor: Colors.transparent,
-              minimumSize: const Size(60,60),
+              minimumSize: const Size(60, 60),
               shape: const CircleBorder()),
-            child: const Icon(Icons.skip_previous,color: Colors.white,)),
-            ElevatedButton(
-            onPressed: () {
-              //pausePressed = true;
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.transparent,
-              minimumSize: const Size(60,60),
-              shape: const CircleBorder()),
-            child: const Icon(Icons.play_arrow,color: Colors.white,)),
-            ElevatedButton(
+          child: Icon(
+            pausePressed ? Icons.pause : Icons.play_arrow,
+            color: Colors.white,
+          ),
+        ),
+        ElevatedButton(
             onPressed: () {},
             style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.transparent,
-              minimumSize: const Size(60,60),
-              shape: const CircleBorder()),
-            child: const Icon(Icons.shuffle,color: Colors.white,)),     
-            ElevatedButton(
+                backgroundColor: Colors.transparent,
+                minimumSize: const Size(60, 60),
+                shape: const CircleBorder()),
+            child: const Icon(
+              Icons.shuffle,
+              color: Colors.white,
+            )),
+        ElevatedButton(
             onPressed: () {
-             TinderCard.controller.next(swipeDirection: SwipeDirection.right,duration: const Duration(milliseconds: 750));
+              TinderCard.controller.next(
+                  swipeDirection: SwipeDirection.right,
+                  duration: const Duration(milliseconds: 750));
             },
             style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.transparent,
-              minimumSize: const Size(60,60),
-              shape: const CircleBorder()),
-            child: const Icon(Icons.skip_next,color: Colors.white,)),
+                backgroundColor: Colors.transparent,
+                minimumSize: const Size(60, 60),
+                shape: const CircleBorder()),
+            child: const Icon(
+              Icons.skip_next,
+              color: Colors.white,
+            )),
       ],
     );
   }
