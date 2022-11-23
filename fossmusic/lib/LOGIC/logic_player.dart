cb89de144ac41e78e374a83f152905ebfec2d7e2
James <entreur@gmail.com>
2022-11-23 12:05:22 +0400

    Pause,play,next,previous song logic.

--- fossmusic/lib/LOGIC/logic_player.dart
+++ fossmusic/lib/LOGIC/logic_player.dart
@@ -2,12 +2,24 @@
 
 import 'package:assets_audio_player/assets_audio_player.dart';
 
-void playSong(String url) {
-  Audio.network(url);
-  //audioPlayer.play(url);
+var _aPlayer = AssetsAudioPlayer.newPlayer();
+
+void startSong(String url) {
+  _aPlayer.open(Audio.network(url));
+}
+
+void continueSong() {
+  _aPlayer.play();
 }
 
 void pauseSong() {
-  //Audio
-  // audioPlayer.pause();
+  _aPlayer.pause();
+}
+
+void nextSong() {
+  _aPlayer.next();
+}
+
+void previousSong() {
+  _aPlayer.previous();
 }
