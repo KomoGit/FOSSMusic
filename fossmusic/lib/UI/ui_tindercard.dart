017aff68c7e13630417635a3cea3cf576a648b0f
James <entreur@gmail.com>
2022-11-23 14:20:50 +0400

    Small change

--- fossmusic/lib/UI/ui_tindercard.dart
+++ fossmusic/lib/UI/ui_tindercard.dart
@@ -1,30 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:swipable_stack/swipable_stack.dart';
 
-// static final controller = SwipableStackController();
-//With each change to code base, the card stack becomes unresponsive during debugging. Decouple UI from main thread.
-
 class TinderCard extends StatelessWidget {
   static final controller = SwipableStackController();
   const TinderCard({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.transparent,
-        body: SwipableStack(
-          builder: (context, index) {
-            return Image.asset("assets/WallpaperDog-20521639.jpg"); //REMOVE ME
-            //return Image.network(Player.audioPlayer.);
-          },
-          controller: controller,
-          detectableSwipeDirections: const {
-            SwipeDirection.right,
-            SwipeDirection.left
-          },
-          onSwipeCompleted: ((index, direction) {
-            debugPrint("$index,$direction");
-          }),
-        ));
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.transparent,
+      body: SwipableStack(
+        builder: (context, index) {
+          return Image.asset("assets/WallpaperDog-20521639.jpg"); //REMOVE ME
+        },
+        controller: controller,
+        detectableSwipeDirections: const {
+          SwipeDirection.right,
+          SwipeDirection.left
+        },
+        onSwipeCompleted: ((index, direction) {
+          debugPrint("$index,$direction");
+        }),
+      ),
+    );
   }
 }
