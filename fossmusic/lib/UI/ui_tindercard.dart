df13eda8199c4f6cfde9d2c3de5ecb231144d2f0
James <entreur@gmail.com>
2022-11-27 12:31:29 +0400

    Switch from Statless to Stateful

--- fossmusic/lib/UI/ui_tindercard.dart
+++ fossmusic/lib/UI/ui_tindercard.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:fossmusic/LOGIC/logic_player.dart';
 import 'package:swipable_stack/swipable_stack.dart';
 
-class TinderCard extends StatelessWidget {
-  static final controller = SwipableStackController();
+class TinderCard extends StatefulWidget {
   const TinderCard({super.key});
+  static final controller = SwipableStackController();
+
+  @override
+  State<TinderCard> createState() => _TinderCardState();
+}
+
+class _TinderCardState extends State<TinderCard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +23,7 @@ class TinderCard extends StatelessWidget {
           }
           return Image.asset("assets/WallpaperDog-20521641.jpg"); //REMOVE ME
         },
-        controller: controller,
+        controller: TinderCard.controller,
         detectableSwipeDirections: const {
           SwipeDirection.right,
           SwipeDirection.left
@@ -29,3 +35,31 @@ class TinderCard extends StatelessWidget {
     );
   }
 }
+
+// class TinderCard extends StatelessWidget {
+//   static final controller = SwipableStackController();
+//   const TinderCard({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       backgroundColor: Colors.transparent,
+//       body: SwipableStack(
+//         builder: (context, index) {
+//           if (getAlbumCover() != null) {
+//             return Image.asset(getAlbumCover());
+//           }
+//           return Image.asset("assets/WallpaperDog-20521641.jpg"); //REMOVE ME
+//         },
+//         controller: controller,
+//         detectableSwipeDirections: const {
+//           SwipeDirection.right,
+//           SwipeDirection.left
+//         },
+//         onSwipeCompleted: ((index, direction) {
+//           debugPrint("$index,$direction");
+//         }),
+//       ),
+//     );
+//   }
+// }
