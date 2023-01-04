a921b20746828b48649d72888596846458f979e8
James <entreur@gmail.com>
2023-01-04 20:54:13 +0400

    Optimization

--- lib/LOGIC/logic_input.dart
+++ lib/LOGIC/logic_input.dart
@@ -4,8 +4,9 @@ bool checkField(TextEditingController controller) {
   return controller.text.isEmpty ? false : true;
 }
 
+//Incase you get out of range issues, add -1 to controller.length (controller.length -1)
 void clearControllerFields(List<TextEditingController> controller) {
-  for (var i = 0; i < controller.length - 1; i++) {
+  for (var i = 0; i < controller.length; i++) {
     controller[i].clear();
   }
 }
