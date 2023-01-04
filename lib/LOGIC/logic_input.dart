c37674db393d0eb2bafc42db638d55efd8dc7eca
James <entreur@gmail.com>
2023-01-04 19:43:50 +0400

    Code cleanup

--- lib/LOGIC/logic_input.dart
+++ lib/LOGIC/logic_input.dart
@@ -3,3 +3,9 @@ import 'package:flutter/material.dart';
 bool checkField(TextEditingController controller) {
   return controller.text.isEmpty ? false : true;
 }
+
+void clearControllerFields(List<TextEditingController> controller) {
+  for (var i = 0; i < controller.length - 1; i++) {
+    controller[i].clear();
+  }
+}
