2c0f3382560e46aa39fd5872110d7dfc1b1c1ee5
James <entreur@gmail.com>
2023-01-05 22:07:24 +0400

    Maaaan I can't take this shit.

--- lib/UI/ui_input.dart
+++ lib/UI/ui_input.dart
@@ -4,6 +4,7 @@ import 'package:fossmusic/DB/models/model_song.dart';
 import 'package:fossmusic/LOGIC/logic_input.dart';
 
 //0,1,2,3......Name,Artist,Link,Album
+
 List<TextEditingController> _controller =
     List.generate(4, (i) => TextEditingController());
 
@@ -34,31 +35,8 @@ class _UserInputPopUpState extends State<UserInputPopUp> {
                   return value!.isEmpty ? "Value Cannot be empty" : null;
                 },
               ),
-              // child: _userInputField(context, "Insert Song Name",
-              //     checkField(_controller[0]), _controller[0]),
             ),
           ),
-          // Flexible(
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(20.0),
-          //     child: _userInputField(context, "Insert Artist Name",
-          //         checkField(_controller[1]), _controller[1]),
-          //   ),
-          // ),
-          // Flexible(
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(20.0),
-          //     child: _userInputField(context, "Insert Album Name",
-          //         checkField(_controller[2]), _controller[2]),
-          //   ),
-          // ),
-          // Flexible(
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(20.0),
-          //     child: _userInputField(context, "Insert Link",
-          //         checkField(_controller[3]), _controller[3]),
-          //   ),
-          // ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Builder(
@@ -86,13 +64,3 @@ class _UserInputPopUpState extends State<UserInputPopUp> {
     );
   }
 }
-
-// Widget _userInputField(
-//     BuildContext context, String label, bool fieldCheck, controller) {
-//   return TextField(
-//       onChanged: (v) => controller.text = v,
-//       decoration: InputDecoration(
-//           contentPadding: const EdgeInsets.all(10),
-//           labelText: label,
-//           errorText: _controller.isNotEmpty ? "Value Cannot be empty" : null));
-// }
